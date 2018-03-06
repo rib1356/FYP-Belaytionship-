2977c70d74000fdc2101671938261d63fe0d6b50
Rob Brown <rib1356@hotmail.co.uk>
2018-03-06 00:25:34 +0000

    Inital layout

--- app/src/main/java/rob_pc/fypbelaytionship/MainPage.java
+++ app/src/main/java/rob_pc/fypbelaytionship/MainPage.java
@@ -3,11 +3,11 @@ package rob_pc.fypbelaytionship;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SignUpPage extends AppCompatActivity {
+public class MainPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up_page);
+        setContentView(R.layout.activity_main_page);
     }
 }
