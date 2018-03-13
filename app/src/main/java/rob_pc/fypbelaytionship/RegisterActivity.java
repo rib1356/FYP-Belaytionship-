fab8d6e8a249a8bfbd72e9af5a06f48b13131fd9
Rob Brown <rib1356@hotmail.co.uk>
2018-03-13 13:01:55 +0000

    Registration working and pushing up to db Login needs to be completed

--- app/src/main/java/rob_pc/fypbelaytionship/RegisterActivity.java
+++ app/src/main/java/rob_pc/fypbelaytionship/RegisterActivity.java
@@ -1,13 +1,69 @@
 package rob_pc.fypbelaytionship;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
+
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+
+        //final Button bLogin = (Button) findViewById(R.id.bLogin);
+        final Button bRegister = (Button) findViewById(R.id.bRegister);
+
+
+        bRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) { //Happens when the response has been executed
+                        try {
+                            Log.d("test", response);
+                            JSONObject jsonResponse = new JSONObject(response); //Gets the string and converts into JSON
+                            boolean success = jsonResponse.getBoolean("success");
+
+                            if(success){
+                                System.out.println("im hereYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY");
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                RegisterActivity.this.startActivity(intent);
+                            }else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                builder.setMessage("Register Failed")
+                                       .setNegativeButton("Retry", null).create().show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+
+                RegisterRequest registerRequest = new RegisterRequest(username, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+            }
+        });
     }
 }
