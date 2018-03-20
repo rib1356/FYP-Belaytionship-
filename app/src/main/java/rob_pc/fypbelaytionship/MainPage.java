0d52ea1897ee7f9a2f729f7c3047b010d89c491d
Rob Brown <rib1356@hotmail.co.uk>
2018-03-20 22:46:20 +0000

    SharedPreferences added
    When a user logs in they will stay logged in until the logout button is pressed
    Save button on profile now does something

--- app/src/main/java/rob_pc/fypbelaytionship/MainPage.java
+++ app/src/main/java/rob_pc/fypbelaytionship/MainPage.java
@@ -1,6 +1,7 @@
 package rob_pc.fypbelaytionship;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,23 +16,39 @@ public class MainPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
 
-
-       // final EditText etUsername = (EditText) findViewById(R.id.etUsername);
         final TextView tvWelcome = findViewById(R.id.tvWelcome);
         final Button bProfile = findViewById(R.id.bProfile);
+        final Button bLogout = findViewById(R.id.bLogout);
 
+        String username = "";
 
-        Intent intent = getIntent();
-        String username = intent.getStringExtra("username");
+        final SharedPreferences prefs = getSharedPreferences("MyPref", MODE_PRIVATE);
+        String restoredText = prefs.getString("username", null); //Get username
+        if (restoredText != null) {
+            username = prefs.getString("username", "No username defined"); //"No name defined" is the default value.
+        }
 
         String message = username + " Welcome bitch";
         tvWelcome.setText(message);
-       // etUsername.setText(username);
 
         bProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainPage.this, UserProfile.class);
+                MainPage.this.startActivity(intent);
+            }
+        });
+
+        bLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //SharedPreferences prefs = getSharedPreferences("MyPref", MODE_PRIVATE);  Should this be used rather than declaring final higher in code?
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.clear(); //Clear all of the shared preference data when the user logs out. This will stop them from automatically logging back in
+                editor.commit(); // commit changes
+
+                Intent intent = new Intent(MainPage.this, LoginActivity.class);
                 MainPage.this.startActivity(intent);
             }
         });
