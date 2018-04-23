6028fd72614351171ffa5bd719e99be420154e75
Rob Brown <rib1356@hotmail.co.uk>
2018-04-24 00:56:56 +0100

    Started the creation of the map page. Currently having issues with getting location

--- app/src/main/java/rob_pc/fypbelaytionship/MainPage.java
+++ app/src/main/java/rob_pc/fypbelaytionship/MainPage.java
@@ -1,15 +1,29 @@
 package rob_pc.fypbelaytionship;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainPage extends AppCompatActivity {
+
+    private static final String TAG = "MainPage";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +33,7 @@ public class MainPage extends AppCompatActivity {
         final TextView tvWelcome = findViewById(R.id.tvWelcome);
         final Button bProfile = findViewById(R.id.bProfile);
         final Button bLogout = findViewById(R.id.bLogout);
+        final Button bMap = findViewById(R.id.bMaps);
 
         String username = "";
 
@@ -53,7 +68,67 @@ public class MainPage extends AppCompatActivity {
             }
         });
 
-
+        if(isServicesOK()){
+            init();
+            statusCheck();
+        }
 
     }
+
+    private void init(){
+        Button bMap = findViewById(R.id.bMaps);
+        bMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainPage.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public boolean isServicesOK() {
+        Log.d(TAG, "isServicesOK: Checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainPage.this);
+        if (available == ConnectionResult.SUCCESS) {
+            Log.d(TAG, "isServicesOK: Google play services is working");
+            return true;
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
+            //An error occured but can be resolved
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainPage.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "You cant make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+
+    public void statusCheck() {
+        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageNoGps();
+
+        }
+    }
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+
 }
